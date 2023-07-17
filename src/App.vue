2d8f14090d2743b354636bf32b901269855ad109
KuligKamil <kamil.marek.kulig@gmail.com>
2023-07-17 23:29:47 +0200

    🔥 dirty: start work on movie form modal

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,21 @@
 import { ref } from "vue";
 import { items } from "./movies.json";
 import MovieList from "@/views/MovieList.vue"
+import Modal from "@/components/Modal.vue"
 
 const movies = ref(items)
+const isOpen = ref(true)
 
 </script>
 
 <template class="">
+  <dir>
+    <button class="rounded-full">Add Movie</button>
+  </dir>
   <div class="mx-auto max-w-7xl">
     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 mx-0 max-w-none lg:grid-cols-3">
       <MovieList :movies="movies" />
     </div>
   </div>
+  <Modal :isOpen="isOpen" />
 </template>
---
+++ src/components/Modal.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div v-if="true" class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+        <!--
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0"
+  -->
+        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+
+        <div class="fixed inset-0 z-10 overflow-y-auto">
+            <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <!--
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      -->
+                <div
+                    class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                    <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+                    <form class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                        <div class="space-y-12">
+                            <div class="pb-12">
+                                <h2 class="text-base font-semibold leading-7 text-gray-900">New Movie</h2>
+                                <p class="mt-1 text-sm leading-6 text-gray-600">This information will be displayed publicly
+                                    so be careful what you share.</p>
+
+                                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                    <div class="sm:col-span-4">
+                                        <label for="username"
+                                            class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+                                        <div class="mt-2">
+                                            <div
+                                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                <span
+                                                    class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
+                                                <input type="text" name="username" id="username" autocomplete="username"
+                                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                    placeholder="janesmith">
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-span-full">
+                                        <label for="about"
+                                            class="block text-sm font-medium leading-6 text-gray-900">Description</label>
+                                        <div class="mt-2">
+                                            <textarea id="about" name="about" rows="3"
+                                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="sm:col-span-4">
+                                        <label for="username"
+                                            class="block text-sm font-medium leading-6 text-gray-900">Image</label>
+                                        <div class="mt-2">
+                                            <div
+                                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                                <span
+                                                    class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
+                                                <input type="text" name="username" id="username" autocomplete="username"
+                                                    class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                                    placeholder="janesmith">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="sm:col-span-4">
+                                        <label for="country"
+                                            class="block text-sm font-medium leading-6 text-gray-900">Generes</label>
+                                        <div class="mt-2">
+                                            <select multiple id="country" name="country" autocomplete="country-name"
+                                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                                <option>United States</option>
+                                                <option>Canada</option>
+                                                <option>Mexico</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="sm:col-span-4">
+                                        <div class="mt-6 space-y-6">
+                                            <div class="relative flex gap-x-3">
+                                                <div class="flex h-6 items-center">
+                                                    <input id="comments" name="comments" type="checkbox"
+                                                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+                                                </div>
+                                                <div class="text-sm leading-6">
+                                                    <label for="comments" class="font-medium text-gray-900">Comments</label>
+                                                    <p class="text-gray-500">Get notified when someones posts a comment on a
+                                                        posting.</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mt-6 flex items-center justify-end gap-x-6">
+                                    <button type="button"
+                                        class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                                    <button type="submit"
+                                        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
