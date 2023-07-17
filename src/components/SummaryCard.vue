5ebc06f33a63a00afd0e9059942e8fbdffa3927e
KuligKamil <kamil.marek.kulig@gmail.com>
2023-07-17 22:16:28 +0200

    🔩 chore: improve the rating UX, add on image rating to be more highlight

--- src/components/SummaryCard.vue
+++ src/components/SummaryCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import RatingButton from "@/components/RatingButton.vue";
+import { StarIcon } from "@heroicons/vue/24/solid";
 
 const props = defineProps<{
     id: number,
@@ -20,8 +21,15 @@ function onClickRate(rating: number) {
 </script>
 
 <template>
-    <article class="flex max-w-xl flex-col items-start justify-between rounded-md bg-white">
-        <div class="overflow-hidden max-h-[500px] w-full">
+    <article class="flex max-w-xl flex-col items-start justify-between rounded-md bg-white static">
+        <div class="overflow-hidden max-h-[500px] w-full relative">
+            <div class="absolute top-0 right-0">
+                <div class="relative flex justify-center items-center">
+                    <StarIcon class="h-16" :class="[ratingActual ? 'text-yellow-500' : 'text-gray-500']" />
+                    <span class="absolute">{{ ratingActual }}</span>
+                </div>
+
+            </div>
             <img :src="image" alt="name" class="h-full w-full object-cover object-center lg:h-full lg:w-full rounded-md"
                 style="height: 550px;">
         </div>
@@ -31,7 +39,7 @@ function onClickRate(rating: number) {
                     {{ name }}
                 </h2>
                 <div class="flex">
-                    <span v-for="gen in genres" :key="`${id}-${genre}`">
+                    <span v-for=" gen  in  genres " :key="`${id}-${genre}`">
                         <span
                             class="inline-flex items-center rounded-xl px-2 py-1 mr-2 text-xs font-medium text-white bg-indigo-500">
                             {{ gen }}
@@ -45,7 +53,7 @@ function onClickRate(rating: number) {
             <div class="mt-8 flex items-center">
                 <p class="text-gray-600 pr-2">Rating: ({{ ratingActual }} / 5 )</p>
                 <div class="flex">
-                    <RatingButton v-for="rate in 5" :ratingActual="ratingActual" :rate="rate"
+                    <RatingButton v-for=" rate  in  5" :ratingActual="ratingActual" :rate="rate"
                         @click:rate="onClickRate(rate)" />
                     <!-- isGraterThanOrEqualTo="ratingActual >= rate"  -->
                 </div>
